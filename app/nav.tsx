0c48bc8a8318698984b31230676770acf10d49f3
tgt <tgt@incognitotgt.me>
2024-11-07 21:28:21 -0500

    miner change

--- app/nav.tsx
+++ app/nav.tsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 import logo from "./icon.png";
 export default function Nav() {
 	return (
-		<nav className="h-16 fixed bg-mantle/20 rounded-md p-4 flex flex-row justify-between border border-mauve m-[1vw] w-[98vw] backdrop-blur-sm">
+		<nav className="h-16 fixed bg-mantle/20 rounded-md p-4 flex flex-row justify-between border border-mauve m-[0.5vw] w-[99vw] backdrop-blur-sm">
 			<div className="flex flex-row h-full gap-2 justify-center items-center">
 				<Image src={logo} className="size-12 rounded-lg" alt="tgt" />
 				<h1 className="text-2xl font-bold text-subtext0">tgt</h1>
