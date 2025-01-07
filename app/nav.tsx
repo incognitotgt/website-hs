0c1289998f00d3de8b33266f09d41915d32fde74
tgt <tgt@incognitotgt.me>
2025-01-06 20:38:32 -0500

    add resume link

--- app/nav.tsx
+++ app/nav.tsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 import logo from "./icon.png";
 export default function Nav() {
 	return (
-		<nav className="h-16 fixed bg-mantle/20 rounded-md p-4 flex flex-row justify-between border border-mauve m-[0.5vw] w-[99vw] backdrop-blur-sm">
+		<nav className="h-16 fixed bg-mantle/20 rounded-md p-4 flex flex-row justify-between border border-mauve m-[0.5vw] w-[99vw] backdrop-blur-sm gap-4">
 			<Link
 				href="/"
 				className="inline-flex justify-center items-center py-4 hover:bg-surface2/70 rounded-md duration-200"
@@ -12,6 +12,13 @@ export default function Nav() {
 				<h1 className="text-2xl font-bold text-subtext0">tgt</h1>
 			</Link>
 			<div className="flex flex-row gap-4 justify-center items-center">
+				<Link
+					className="text-blue hover:text-sky active:text-red font-bold"
+					href="https://ma-personal.notion.site/resume"
+				>
+					resume
+				</Link>
+				/
 				<a
 					href="https://ctp-webr.ing/tgt/previous"
 					className="text-mauve hover:text-mauve/75 active:text-red text-xl font-extrabold"
