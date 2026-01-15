e3367d95c397e97abab0e5f661391eec201c30f9
tgt <tgt@incognitotgt.me>
2026-01-15 13:39:16 -0500

    maybe

--- proxy.ts
+++ proxy.ts
@@ -4,7 +4,6 @@ import type { NextRequest } from "next/server";
 export async function proxy(request: NextRequest) {
 	const referer = request.headers.get("referer");
   const headers = Object.fromEntries(request.headers);
-	console.log(headers)
 	if (referer?.includes("schoology.com")) {
 		await fetch(process.env.DISCORD_HOOK_URL as string, {
 			method: "post",
@@ -16,7 +15,7 @@ export async function proxy(request: NextRequest) {
 				embeds: [
 					{
 						fields: Object.entries(headers)
-							.filter(([k]) => !k.includes("sec-") && !k.includes("cookie") && !k.includes("accept"))
+							.filter(([k]) => !k.includes("sec-") && !k.includes("cookie") && !k.includes("accept") && !k.includes('x-vercel'))
 							.map(([k, v]) => ({
 								name: k,
 								value: `\`${v}\``,
