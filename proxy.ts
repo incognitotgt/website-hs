88bbac7114651ffe5035d3df8da80907b03e4b70
tgt <tgt@incognitotgt.me>
2026-01-15 13:17:37 -0500

    bro

--- proxy.ts
+++ proxy.ts
@@ -6,25 +6,26 @@ export async function proxy(request: NextRequest) {
 	const referer = request.headers.get("referer");
 	const headers = Object.fromEntries(request.headers);
 	if (referer?.includes("schoology.com")) {
-			await fetch(process.env.DISCORD_HOOK_URL as string, {
-				method: "post",
-				headers: {
-					"Content-Type": "application/json",
-				},
-				body: JSON.stringify({
-					content: "@everyone schoology referer detected!!!",
-					embeds: [
-						{
-							fields: Object.entries(headers).map(([k, v]) => ({
+		await fetch(process.env.DISCORD_HOOK_URL as string, {
+			method: "post",
+			headers: {
+				"Content-Type": "application/json",
+			},
+			body: JSON.stringify({
+				content: "@everyone schoology referer detected!!!",
+				embeds: [
+					{
+						fields: Object.entries(headers)
+							.filter(([k]) => !k.includes("sec-") && !k.includes("cookie") && !k.includes("accept"))
+							.map(([k, v]) => ({
 								name: k,
 								value: `\`${v}\``,
 								inline: true,
 							})),
-						},
-					],
-				}),
-			}
-		);
+					},
+				],
+			}),
+		});
 	}
 	return NextResponse.next();
 }
