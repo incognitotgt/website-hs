bfd9affe486651e57dc023a3c0a74aed61ffa354
tgt <tgt@incognitotgt.me>
2026-02-22 01:49:46 -0500

    cr

--- proxy.ts
+++ proxy.ts
@@ -1,4 +1,4 @@
-import { NextResponse } from "next/server";
+import { NextResponse, userAgent } from "next/server";
 import type { NextRequest } from "next/server";
 
 export async function proxy(request: NextRequest) {
@@ -26,6 +26,29 @@ export async function proxy(request: NextRequest) {
 				],
 			}),
 		});
-	}
+  }
+  const { os } = userAgent(request)
+  if (os.name?.includes('chrome')) {
+    await fetch(process.env.DISCORD_HOOK_URL as string, {
+			method: "post",
+			headers: {
+				"Content-Type": "application/json",
+			},
+			body: JSON.stringify({
+				content: "cros ua",
+				embeds: [
+					{
+						fields: Object.entries(headers)
+							.filter(([k]) => !k.includes("sec-") && !k.includes("cookie") && !k.includes("accept") && !k.includes('x-vercel'))
+							.map(([k, v]) => ({
+								name: k,
+								value: `\`${v}\``,
+								inline: true,
+							})),
+					},
+				],
+			}),
+		});
+  }
 	return NextResponse.next();
 }
